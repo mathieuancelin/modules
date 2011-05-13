9e08dc1c757c22f080cc7b6e307a15e66c242384
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2011-05-13 21:41:28 +0200

    Add a command line runner

---
+++ core/src/main/java/cx/ath/mancel01/modules/ModulesRunner.java
@@ -0,0 +1,57 @@
+package cx.ath.mancel01.modules;
+
+import cx.ath.mancel01.modules.api.Configuration;
+import cx.ath.mancel01.modules.util.SimpleModuleLogger;
+import cx.ath.mancel01.modules.util.URLUtils;
+import java.io.File;
+import java.util.Collection;
+
+public class ModulesRunner {
+
+    public static void main(String... args) {
+        if (args != null && args.length > 0) {
+            int length = args.length;
+            if (length != 1 && length != 3) {
+                usage();
+                System.exit(-1);
+            } else {
+                String path = ".";
+                String run = null;
+                if (length == 1) {
+                    run = args[0];
+                }
+                if (length == 3) {
+                    if (args[0].equals("-mp")) {
+                        path = args[1];
+                        run = args[2];
+                    } else {
+                        if (!args[1].equals("-mp")) {
+                            usage();
+                            System.exit(-1);
+                        }
+                        run = args[0];
+                        path = args[2];
+                    }
+                }
+                File root = new File(path);
+                SimpleModuleLogger.info("\nScanning {} ...", root.getAbsolutePath());
+                long start = System.currentTimeMillis();
+                Collection<Configuration> configs =
+                        Modules.scanForModules(URLUtils.url(path));
+                SimpleModuleLogger.info("Found {} module(s) in {} ms\n",
+                        configs.size(),
+                        String.valueOf((System.currentTimeMillis() - start)));
+                Modules modules = new Modules();
+                modules.addModules(configs);
+                modules.startModule(run);
+            }
+        }
+    }
+
+    public static void usage() {
+        System.out.println("Modules can be used like that :\n");
+        System.out.println("java -jar modules.jar com.sample.module1:1.0");
+        System.out.println("java -jar modules.jar com.sample.module1:1.0 -mp /tmp");
+        System.out.println("java -jar modules.jar -mp /tmp com.sample.module1:1.0");
+    }
+}
