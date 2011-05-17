c15ade4098950c404edc4b515820e3f833f02972
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2011-05-17 22:12:10 +0200

    Add colors to logger (ANSI console)

--- core/src/main/java/cx/ath/mancel01/modules/util/SimpleModuleLogger.java
+++ core/src/main/java/cx/ath/mancel01/modules/util/SimpleModuleLogger.java
@@ -4,30 +4,58 @@ import java.io.PrintStream;
 
 public class SimpleModuleLogger {
 
+    private static final int NORMAL = 0;
+    private static final int BRIGHT = 1;
+    private static final int FOREGROUND_BLACK = 30;
+    private static final int FOREGROUND_RED = 31;
+    private static final int FOREGROUND_GREEN = 32;
+    private static final int FOREGROUND_YELLOW = 33;
+    private static final int FOREGROUND_BLUE = 34;
+    private static final int FOREGROUND_MAGENTA = 35;
+    private static final int FOREGROUND_CYAN = 36;
+    private static final int FOREGROUND_WHITE = 37;
+    private static final String PREFIX = "\u001b[";
+    private static final String SUFFIX = "m";
+    private static final char SEPARATOR = ';';
+    private static final String END_COLOUR = PREFIX + SUFFIX;
+    private static final String FATAL_COLOUR = PREFIX + BRIGHT + SEPARATOR + FOREGROUND_RED + SUFFIX;
+    private static final String ERROR_COLOUR = PREFIX + NORMAL + SEPARATOR + FOREGROUND_RED + SUFFIX;
+    private static final String WARN_COLOUR = PREFIX + NORMAL + SEPARATOR + FOREGROUND_YELLOW + SUFFIX;
+    private static final String INFO_COLOUR = PREFIX + SUFFIX;
+    private static final String DEBUG_COLOUR = PREFIX + NORMAL + SEPARATOR + FOREGROUND_CYAN + SUFFIX;
+    private static final String TRACE_COLOUR = PREFIX + NORMAL + SEPARATOR + FOREGROUND_BLUE + SUFFIX;
     private static final String TRACE_PREFIX = "[TRACE] ";
 
     private static boolean trace = false;
+    private static boolean colors = false;
 
     public static void enableTrace(boolean trace) {
         SimpleModuleLogger.trace = trace;
     }
 
+    public static void enableColors(boolean colors) {
+        SimpleModuleLogger.colors = colors;
+    }
+
     public static void error(String message, Object... printable) {
-        print(System.err, message, printable);
+        print(ERROR_COLOUR, System.err, message, printable);
     }
 
     public static void info(String message, Object... printable) {
-        print(System.out, message, printable);
+        print(INFO_COLOUR, System.out, message, printable);
     }
 
     public static void trace(String message, Object... printable) {
         if (trace) {
-            print(System.out, TRACE_PREFIX + message, printable);
+            print(TRACE_COLOUR, System.out, TRACE_PREFIX + message, printable);
         }
     }
 
-    private static void print(PrintStream out, String message, Object... printable) {
+    private static void print(String color, PrintStream out, String message, Object... printable) {
         StringBuilder print = new StringBuilder();
+        if (colors) {
+            print.append(color);
+        }
         if (message.contains("{}") && printable != null && printable.length > 0) {
             String[] parts = message.split("\\{\\}");
             int i = 0;
@@ -53,6 +81,9 @@ public class SimpleModuleLogger {
                 print.append(o.toString());
                 print.append("\n");
             }
+        }
+        if (colors) {
+            print.append(END_COLOUR);
         }
         out.println(print.toString());
     }
